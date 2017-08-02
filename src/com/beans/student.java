e24946319e7fd294c6ef42f58334d6c48e99d3f5
ahmedmar3y2017 <ahmedmar3y108@gmail.com>
2017-08-02 14:58:53 +0300

    init

---
+++ src/com/beans/student.java
@@ -0,0 +1,29 @@
+package com.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class student {
+	
+	private String firstName;
+	private String lastName;
+	public student() {
+		super();
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+
+}
