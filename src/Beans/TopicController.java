b7f96248825840474740904dd554b788caa9c8be
ahmedmar3y2017 <ahmedmar3y108@gmail.com>
2017-08-05 05:56:09 +0300

    uibi

--- src/Beans/TopicController.java
+++ src/Beans/TopicController.java
@@ -9,53 +9,44 @@ import javax.faces.bean.SessionScoped;
 
 import Entities.Topics;
 
-@ManagedBean(name="topics")
+@ManagedBean(name = "topics")
 @SessionScoped
 public class TopicController {
-	
-	List<Topics> topics=null;
-	
+
+	List<Topics> topics = null;
+
 	Topics selectedTopics;
-	
+
 	@PostConstruct
 	public void init() {
-		topics=new ArrayList<>();
+		topics = new ArrayList<>();
 		topics.add(new Topics("java", "java", "java"));
 		topics.add(new Topics("c++", "c++", "c++"));
 		topics.add(new Topics("php", "php", "php"));
-		selectedTopics=new Topics();
-		
+		selectedTopics = new Topics();
+
 	}
 
 	public void select(Topics topics) {
-this.selectedTopics=topics;	
+		this.selectedTopics = topics;
 	}
+
 	public void delete() {
-topics.remove(selectedTopics);
+		topics.remove(selectedTopics);
 	}
-	
+
 	public void update() {
-this.selectedTopics=new Topics();
+		this.selectedTopics = new Topics();
 	}
-	
-	
-	
-	
+
 	public Topics getSelectedTopics() {
 		return selectedTopics;
 	}
-
-
-
 
 	public void setSelectedTopics(Topics selectedTopics) {
 		this.selectedTopics = selectedTopics;
 	}
 
-
-
-
-	
 	public List<Topics> getTopics() {
 		return topics;
 	}
@@ -63,6 +54,5 @@ this.selectedTopics=new Topics();
 	public void setTopics(List<Topics> topics) {
 		this.topics = topics;
 	}
-	
 
 }
--- src/Service/topicService.java
+++ src/Service/topicService.java
@@ -2,40 +2,105 @@ package Service;
 
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 
 import Entities.HibernateUtil;
 import Entities.Topics;
 
 public class topicService {
-	
-//	public static void main(String [] args) {
-//		
-//		new topicService().getAll();
-//		
-//	}
-private List<Topics>  getAll() {
-	// TODO Auto-generated method stub
 
-	 Session session = HibernateUtil.getSessionFactory().openSession();
-	    
-     session.beginTransaction();
-     Query q = session.createQuery("From Topics ");
-     
-     List<Topics> resultList = q.list();
-     System.out.println("num of Topics :" + resultList.size());
-     for (Topics next : resultList) {
-         System.out.println( next.getUsername());
-     }
-		
-   
-     session.getTransaction().commit();
+	public static void main(String[] args) {
 
-session.close();
-		
-return resultList;
+//		 new topicService().insert(new Topics("1", "ahmed", "ahmed"));
+//		new topicService().delete(new Topics("1", "ahmed", "ahmed"));
+		// new topicService().update(new Topics("ahmed", "mohamed", "eslam"));
+
+		new topicService().getAll();
+	}
+
+	private void update(Topics topics) {
+		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+
+		session.saveOrUpdate(topics);
+
+		session.getTransaction().commit();
+
+		session.close();
+	}
+
+	private List<Topics> getAll() {
+		// TODO Auto-generated method stub
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		Query q = session.createQuery("From Topics ");
+
+		List<Topics> resultList = q.list();
+		System.out.println("num of Topics :" + resultList.size());
+		for (Topics next : resultList) {
+			System.out.println(next.getUsername());
+		}
+
+		session.getTransaction().commit();
+
+		session.close();
+
+		return resultList;
+
+	}
+
+	public void insert(Topics topics) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+
+		session.save(topics);
+
+		session.getTransaction().commit();
+
+		session.close();
+
+	}
+
+	public void delete(Topics topics) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+
+		Query query =session.createQuery("From Topics where id=:id");
+		query.setParameter("id", topics.getId());
+		Topics oo =(Topics) query.uniqueResult();
 	
-}
+
+		if (oo != null) {
+	
+			System.out.println("Done");
+			Query query1 =session.createQuery("delete from Topics where id=:id");
+			query1.setParameter("id", oo.getId());
+			
+			query1.executeUpdate();
+	
+
+		}
+		
+		
+		session.getTransaction().commit();
+	
+		session.close();
+
+		
+		
+		
+
+	}
 
 }
